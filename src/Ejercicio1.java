50fe180972a95b5fae5bba58ec1696ff9a4c54b1
Inma <irammar747@g.educaand.es>
2023-05-24 13:27:01 +0200

    eje1Terminado

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,12 +1,32 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(new FileReader("entrada.txt"))) {
+        try(Scanner sc = new Scanner(new FileReader("entrada.txt"));
+            PrintWriter pw = new PrintWriter(new FileWriter("salida.txt"))) {
+            while(sc.hasNext()){
+                String nombreDia = sc.next();
+                int sumaTem = 0;
+                int temp;
+                int numTemp = 0;
+                while(true){
+                    temp = sc.nextInt();
+                    if(temp == -1) break;
+                    sumaTem += temp;
+                    numTemp++;
+                }
+                if(numTemp == 0){
+                    pw.println(String.format("%s: No hay datos", nombreDia));
+                }else{
+                    double r = (double)sumaTem/numTemp;
+                    pw.println(String.format("%s: %.2f", nombreDia, r));
+                }
 
+            }
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 public class Ejercicio2 {
     private final static int TAM_REG = (50*2+2) + 1 + Integer.BYTES + Double.BYTES;
     private final static int TAM_CABECERA = Integer.BYTES;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         File f = new File("datos.dat");
         if(!f.exists()){
             System.out.println("Creando el fichero...");
@@ -19,13 +21,46 @@ public class Ejercicio2 {
             }
         }else{
             try(RandomAccessFile raf = new RandomAccessFile("entrada.dat", "rw")){
-                int opcion;
+                boolean salir = false;
+                while(!salir){
+                    switch (mostrarMenu(sc)){
+                        case 1:
+                            introducirPeso(raf, sc);
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 0:
+                            salir = true;
+                            break;
+                        default:
+                            System.out.println("Opción incorrecta");
+                    }
+                }
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+
+    }
+    public static int mostrarMenu(Scanner sc){
+        int opcion;
+
+        System.out.println("Menú");
+        System.out.println("1. Introducir peso");
+        System.out.println("2. Mostrar peso");
+        System.out.println("3. Generar Informe");
+        System.out.println("4. Salir");
+
+        opcion = sc.nextInt();
+
+        return opcion;
+
+    }
+    public static void introducirPeso(RandomAccessFile raf, Scanner sc){
 
     }
 }
